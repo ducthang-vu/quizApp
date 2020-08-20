90cb596862ae55402aabbb58f3dc3d8e0787b45e
Duc Thang Vu <vuducthang91@gmail.com>
2020-08-20 12:30:28 +0200

    Updates

---
+++ precache-manifest.a92de20057a94247229b8a07130dd56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ea38db1168cb98d0c5ddd7d1e9ba7f",
+    "url": "/quizzApp/index.html"
+  },
+  {
+    "revision": "872f4840dbf823f418d3",
+    "url": "/quizzApp/static/css/main.f8e5196f.chunk.css"
+  },
+  {
+    "revision": "20cc3f9ed6202acbb005",
+    "url": "/quizzApp/static/js/2.d932584d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizzApp/static/js/2.d932584d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "872f4840dbf823f418d3",
+    "url": "/quizzApp/static/js/main.2b9a6864.chunk.js"
+  },
+  {
+    "revision": "17543280cfec1a65c808",
+    "url": "/quizzApp/static/js/runtime-main.86ca6900.js"
+  }
+]);
